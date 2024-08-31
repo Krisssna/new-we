ccebef7ac5ced02bb3a2fa5eb029f5204533fb3f
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-08-31 16:49:20 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getelementByID('nav-menu'),
+  navToggle = document.getElementByID('nav-toggle'),
+  navClose = document.getElementByID('nav-close'),
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+  navToggle.addEventListner('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
