aeb36ca321fb0ce5e7ea798399e8c778d42d5e61
Krisssna <92026573+Krisssna@users.noreply.github.com>
2024-08-31 17:22:46 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getelementByID('nav-menu'),
   navToggle = document.getElementByID('nav-toggle'),
-  navClose = document.getElementByID('nav-close'),
+  navClose = document.getElementByID('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
